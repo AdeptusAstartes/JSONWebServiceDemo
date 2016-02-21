2cfa8ac3147fc26f0c781855cb36565a5c77bf25
Don Angelillo <don.angelillo@newsela.com>
2016-02-21 01:34:30 -0500

    ok actually added Alamofire this time

---
+++ JSONWebServiceDemo/Managers/FeedManager.swift
@@ -0,0 +1,14 @@
+//
+//  FeedManager.swift
+//  JSONWebServiceDemo
+//
+//  Created by Donald Angelillo on 2/21/16.
+//  Copyright © 2016 Donald Angelillo. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class FeedManager {
+
+}
